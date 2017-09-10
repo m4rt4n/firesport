547d82660bc634030c79a5a718bb513166848bec
m4rt4n <martan84@centrum.cz>
2017-09-10 17:10:59 +0200

    nějaké nové poznámky + začátek práce

---
+++ firesport/fireattack.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+###############################################################################
+#
+#	Author: Martin Bačo
+#	Name: FiresportAttack
+#	verze: 0.1
+#
+#	Description:
+#		Simulátor hasičské sportovní disciplíny "Požární útok"
+#simulátor k výpočtu časů v požárním útoku
+#
+###############################################################################
+
